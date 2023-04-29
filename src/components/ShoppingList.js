2cb51388fc5f31a4bca5aa1b8b4e894ab4f0e2d3
vincenttommi <vincenttommi@gmail.com>
2023-04-29 14:38:42 +0300

    code crud react

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
 import ItemForm from "./ItemForm";
 import Filter from "./Filter";
 import Item from "./Item";
@@ -6,6 +7,17 @@ import Item from "./Item";
 function ShoppingList() {
   const [selectedCategory, setSelectedCategory] = useState("All");
   const [items, setItems] = useState([]);
+
+// adding use effect hook to fetch data from  json api
+
+useEffect(() =>{
+  fetch("http://localhost:4000/items")
+     .then((r) => r.json())
+     .then((items) => console.log(items))
+
+
+
+},[]);
 
   function handleCategoryChange(category) {
     setSelectedCategory(category);
@@ -31,6 +43,6 @@ function ShoppingList() {
       </ul>
     </div>
   );
-}
+} 
 
 export default ShoppingList;
